87fa8d03de34b1bd677148ad5420f616f8b319d6
thebluetropics <thebluetropics@gmail.com>
2025-10-06 13:41:26 +0700

    fix `compile_and_remap_tests.sh`

--- scripts/compile_and_remap_tests.sh
+++ scripts/compile_and_remap_tests.sh
@@ -2,7 +2,7 @@
 
 for dir in ./test/*; do
   mkdir -p ${dir%/}/out
-  rm -f ${dir%/}/out/*
+  rm -rf ${dir%/}/out/*
   javac -d ${dir%/}/out ${dir%/}/src/*.java
   java -cp deps/*:out com.thebluetropics.crabremapper.CrabRemapper -m ${dir%/}/mappings.baked remap ${dir%/}/out
 done
